99db1ea321d6cdbfa05a549591b651061db72652
Fazan4ik <jagannath2006@outlook.com>
2024-02-19 19:12:11 +0200

    Add project files.

---
+++ Notes/Controllers/NotesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    public class NotesController : Controller
+    {
+        private static string Filename = "notes.txt";
+        private static Random Rand = new();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var defNotes = new List<Note>();
+            for (int i = 0; i < Rand.Next(2, 10); i++)
+            {
+                defNotes.Add(new()
+                {
+                    Title = $"Note {Rand.Next(1, 100)}",
+                    Text = $"Text for {i + 1} note",
+                    CreateDT = DateTime.Now,
+                    Tags = new[] { $"#Tag{Rand.Next(1, 100)}", $"#Tag{Rand.Next(1, 100)}" }
+                });
+            }
+            return View("Index", defNotes);  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveInFile([FromBody] List<Note> notes)
+        {
+            try
+            {
+                JsonSerializerOptions options = new()
+                {
+                    WriteIndented = true  
+                };
+                string jsonString = JsonSerializer.Serialize(notes, options);
+                await System.IO.File.WriteAllTextAsync(Filename, jsonString);
+                return Json(new { success = true, message = "Збережено..." });
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "Помилка..." });
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LoadFromFile()
+        {
+            string jsonString = await System.IO.File.ReadAllTextAsync(Filename);
+            var notesFromFile = JsonSerializer.Deserialize<List<Note>>(jsonString);
+            return PartialView("_NotesPartial", notesFromFile);  
+        }
+    }
+}
---
+++ Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿namespace Notes.Models
+{
+    [Serializable]
+    public class Note
+    {
+        public string Title { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public DateTime CreateDT { get; set; }
+        public string[] Tags { get; set; } = null!;
+
+        public string GetTagsString(string sep = "/")
+        {
+            return String.Join(sep, Tags);
+        }
+    }
+}
